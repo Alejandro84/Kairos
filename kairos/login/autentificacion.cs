f3c43a9adf7c666b346a23c9625d477b4ca2a8ef
Alejandro Vargas <Alejandro84@>
2016-12-05 15:38:59 -0300

    intento de login

---
+++ kairos/login/autentificacion.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Web.Security;
+
+public static class Autentificacion
+{
+    public static bool Autenticar(string usuario, string password)
+    {
+        //consulta a la base de datos
+        string sql = @"SELECT COUNT(*)
+                          FROM Usuarios
+                          WHERE usuario = @user AND contraseña = @pass";
+        //cadena conexion
+        using (SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["default"].ToString()))
+        {
+            conn.Open();//abrimos conexion
+
+            SqlCommand cmd = new SqlCommand(sql, conn); //ejecutamos la instruccion
+            cmd.Parameters.AddWithValue("@user", usuario); //enviamos los parametros
+            cmd.Parameters.AddWithValue("@pass", password);
+
+            int count = Convert.ToInt32(cmd.ExecuteScalar()); //devuelve la fila afectada
+
+            if (count == 0)
+                return false;
+            else
+                return true;
+
+        }
+    }
+}
---
+++ kairos/login/login.aspx
@@ -0,0 +1,19 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="login.aspx.cs" Inherits="kairos.login.login" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        
+        <input id="txtUsuario" type="text" /><br />
+        <input id="txtClave" type="password" /></div>
+        <asp:Button ID="ingresar" runat="server" Text="Ingresar" OnClick="ingresar_Click" />
+    </form>
+</body>
+</html>
