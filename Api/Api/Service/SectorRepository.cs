932e3189e246e3774151c62d5ff0c824bc81ef6a
Andres <a.o.grubsic@gmail.com>
2016-12-06 11:40:20 -0300

    Proyecto api web

---
+++ Api/Api/Service/SectorRepository.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Api.Models;
+
+namespace Api.Service
+{
+    public class SectorRepository
+    {
+        public Sector[] GetAllSectores()
+        {
+            return new Sector[]
+        {
+             new Sector
+             {
+                  Id = 1,
+                  Nombre = "Glenn Block"
+             },
+             new Sector
+             {
+                  Id = 2,
+                  Nombre = "Dan Roth"
+             }
+        };
+        }
+    }
+}
